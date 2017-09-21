971c80f01dc50e7dcba58dd3d2277b4c38c33873
Ben Baum <bbaum@spredfast.com>
2017-09-20 22:11:32 -0400

    Stub in reducers when a new action is added

--- scripts/state/add.js
+++ scripts/state/add.js
@@ -11,12 +11,25 @@ import {
   addExpressionToProgram,
   addShorthandExport
 } from "util/program";
+import toAST from "util/toAST";
 import parse from "../parser";
 import camelCase from "camelcase";
 import generate from "babel-generator";
 import prettier from "prettier";
 
-import type { Project, Program, ExportNamedDeclaration } from "types";
+import type {
+  Project,
+  Program,
+  ExportNamedDeclaration,
+  VariableDeclaration,
+  VariableDeclarator,
+  ObjectProperty
+} from "types";
+
+const getActionConstantName = (action: string): string => action.toUpperCase();
+
+const getActionConstantValue = (state: string, action: string): string =>
+  `${state.toUpperCase()}_${action}`;
 
 const addActionConstant = (
   state: string,
@@ -40,7 +53,7 @@ const addActionConstant = (
 
   if (actionConstantIndex === -1) {
     addExpressionToProgram(
-      `const ${action}: string = "${state.toUpperCase()}_${action}";`,
+      `const ${action}: string = "${getActionConstantValue(state, action)}";`,
       program,
       actionConstantsExportIndex
     );
@@ -57,12 +70,50 @@ const addActionConstant = (
   return program;
 };
 
+const stubReducer = (
+  state: string,
+  action: string,
+  program: Program
+): Program => {
+  const reducersIndex: number = findVariableDeclarationIndex(
+    program,
+    "reducer"
+  );
+  if (reducersIndex === -1) {
+    throw new Error("Cannot find reducers");
+  }
+
+  const reducer: VariableDeclarator = ((program.body[
+    reducersIndex
+  ]: any): VariableDeclaration).declarations[0];
+
+  const actionConstantName: string = getActionConstantName(action);
+
+  const reducerProperties: Array<ObjectProperty> = reducer.init.properties;
+  if (
+    !reducerProperties.find(
+      (property: ObjectProperty): boolean =>
+        property.key.name === actionConstantName
+    )
+  ) {
+    reducer.init.properties.push(
+      toAST(
+        `{[${actionConstantName}]: (state: ${state}, action): ${state} => {
+      return state;
+    }}`
+      ).properties[0]
+    );
+  }
+
+  return program;
+};
+
 export const addAction = (
   state: string,
   action: string,
   config: Project
 ): void => {
-  action = action.toUpperCase();
+  action = getActionConstantName(action);
   const completeStateDir: string = getCompleteStateDir(config);
   createDirIfNeeded(completeStateDir);
 
@@ -110,6 +161,8 @@ export const addAction = (
   addShorthandExport(actionsExport, actionCreatorName);
 
   contents = addActionConstant(state, action, contents);
+
+  contents = stubReducer(state, action, contents);
 
   const newStateString: string = prettier.format(generate(contents).code);
 

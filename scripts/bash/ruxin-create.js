22d549d069d4dc50fff68e20117cef959ac67a6b
Ben Baum <bbaum@spredfast.com>
2017-09-20 21:04:29 -0400

    Make bash scripts js files

--- scripts/bash/ruxin-create.js
+++ scripts/bash/ruxin-create.js
@@ -2,11 +2,10 @@
 var api = require("../../build/bundle");
 var program = require("commander");
 
-program.version("0.0.0")
-  .action(function (name) {
-    api.init({
-      baseDir: process.cwd() + "/" + name
-    })
-  })
+program.version("0.0.0").action(function(name) {
+  api.init({
+    baseDir: process.cwd() + "/" + name
+  });
+});
 
 program.parse(process.argv);

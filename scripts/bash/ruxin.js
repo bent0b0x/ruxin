ce4fbb07e4ea151bcd6d7c532e33f8c67c4ea268
Ben Baum <bbaum@spredfast.com>
2017-10-07 11:31:40 -0400

    19 - rename some cli commands to be more explicit/clear

--- scripts/bash/ruxin.js
+++ scripts/bash/ruxin.js
@@ -4,8 +4,8 @@ var program = require("commander");
 program
   .version("0.0.0")
   .command("create <name>", "initialize project")
-  .command("add <state> [action]", "add to project")
+  .command("generate-state <state> [action]", "add to project")
   .command("add-props <state>", "add prop(s) to a state piece")
-  .command("add-component <component> [state]", "create a new component")
-  .command("add-container <container> [state]", "create a new container")
+  .command("generate-component <component> [state]", "create a new component")
+  .command("generate-container <container> [state]", "create a new container")
   .parse(process.argv);
